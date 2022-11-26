59448b3c96ef2ebc0e09edfb3f678000d47233f6
tengqi <75350626+dwz92@users.noreply.github.com>
2022-11-26 13:08:12 -0500

    [DEV-1.1] Added GoogleMapGui to branch. A static visual of campus map

---
+++ Map/GoogleMapsGui.java
@@ -0,0 +1,127 @@
+/**
+ * A static map of UTSG campus
+ *
+ * Reference from: https://stackoverflow.com/questions/1993981/how-to-access-google-maps-api-in-java-application
+ */
+
+
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class GoogleMapsGui extends JFrame {
+
+    final Logger log = Logger.getLogger(GoogleMapsGui.class.getName());
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GoogleMapsGui frame = new GoogleMapsGui();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public GoogleMapsGui() {
+        setTitle("Map");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1000, 800);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+        JFrame test = new JFrame("Google Maps");
+
+        try {
+            // String latitude = "-25.9994652";
+            // String longitude = "28.3112051";
+//            String location = JOptionPane
+//                    .showInputDialog(" please enter the desired loccation");// get
+            // the
+            // location
+            // for
+            // geo
+            // coding
+//            Scanner sc = new Scanner(location);
+//            Scanner sc2 = new Scanner(location);
+//            String marker = "";
+//            String path = JOptionPane
+//                    .showInputDialog("what is your destination?");
+//            String zoom = JOptionPane
+//                    .showInputDialog("how far in do you want to zoom?\n"
+//                            + "12(zoomed out) - 20 (zoomed in)");
+//
+//            String imageUrl = "https://maps.googleapis.com/maps/api/staticmap?";
+//            while (sc.hasNext()) {// add location to imageUrl
+//                imageUrl = imageUrl + sc.next();
+//            }
+//            marker = "&markers=color:red|";
+//            while (sc2.hasNext()) {// add marker location to marker
+//                marker = marker + sc2.next() + ",";
+//
+//            }
+//            marker = marker.substring(0, marker.length() - 1);
+//
+//            imageUrl = imageUrl + "&size=620x620&scale=2&maptype=hybrid"
+//                    + marker;
+            //
+            log.info("Generated url");
+
+            String destinationFile = "image.jpg";
+
+            // read the map image from Google
+            // then save it to a local file: image.jpg
+            //
+//            URL url = new URL(imageUrl);
+            URL url = new URL("https://maps.googleapis.com/maps/api/staticmap?center=27King's+College+Cir,Toronto,ON&zoom=16&size=800x800&key=AIzaSyBfybhe83kPUTqNm-wWo7S8PUVUeKW7HIg");
+            InputStream is = url.openStream();
+            OutputStream os = new FileOutputStream(destinationFile);
+
+            byte[] b = new byte[2048];
+            int length;
+
+            while ((length = is.read(b)) != -1) {
+                os.write(b, 0, length);
+            }
+            log.info("Created image.jpg");
+
+            is.close();
+            os.close();
+//            sc.close();
+//            sc2.close();
+            log.info("Closed util's");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+            log.severe("Could not create image.jpg");
+        }// fin getting and storing image
+
+        ImageIcon imageIcon = new ImageIcon((new ImageIcon("image.jpg"))
+                .getImage().getScaledInstance(1000, 800,
+                        java.awt.Image.SCALE_SMOOTH));
+        contentPane.setLayout(null);
+        JLabel imgMap = new JLabel(imageIcon);
+        imgMap.setBounds(5, 5, 1000, 800);
+        contentPane.add(imgMap);
+    }
+
+}
