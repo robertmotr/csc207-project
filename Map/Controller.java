01398445fd95bbda862ffd93f9b0b1daecd296f0
iwardah <wardahijaz21@gmail.com>
2022-12-05 22:14:17 -0500

    Finished the filter portion of the search bar

--- Map/Controller.java
+++ Map/Controller.java
@@ -1,29 +1,26 @@
 package Map;
 
 import com.dlsc.gmapsfx.GoogleMapView;
-import com.dlsc.gmapsfx.MapComponentInitializedListener;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+
+import static Map.PlaceInfo.getAnchorsofNamesURL;
+import static Map.PlaceInfo.name;
 
 
 public class Controller implements Initializable, MapComponentInitializedListener {
@@ -83,10 +80,6 @@ public class Controller implements Initializable, MapComponentInitializedListene
             throw new RuntimeException(e);
         }
 
-        File file = new File("C:\\Users\\R_asl\\CSC207\\csc207-project\\resources\\webmap.png");
-        Image image = new Image(file.toURI().toString());
-//        mapImage.setImage(image);
-
         //Search Button
         searchBtn.setOnAction(e -> {
             //if filter not selected, assume its searching for building
@@ -107,10 +100,61 @@ public class Controller implements Initializable, MapComponentInitializedListene
 
         TreeItem<String>  rootItem = new TreeItem<>("Places");
         TreeItem<String> studyItem = new TreeItem<>("Study Places");
+        for (Place place: this.studyplaceInfo){
+            studyItem.getChildren().add(place);
+        }
         TreeItem<String> foodItem = new TreeItem<>("Food Places");
         TreeItem<String> buildingItem = new TreeItem<>("Building Places");
+        for (Place place: this.buildingInfo){
+            buildingItem.getChildren().add(place);
+        }
         TreeItem<String> allFoodItem = new TreeItem<>("All Places");
-        foodItem.getChildren().add(allFoodItem);
+        for (Place place: this.foodplaceInfo) {
+            allFoodItem.getChildren().add(place);
+        }
+        TreeItem<String> halalItem = new TreeItem<>("Halal Places");
+        try {
+            for (HtmlAnchor place: getAnchorsofNamesURL(foodTypeInfo.get("Halal Entrees Available"))){
+                halalItem.getChildren().add(new TreeItem<>(name(place)));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TreeItem<String> beverageItem = new TreeItem<>("Beverage Places");
+        try {
+            for (HtmlAnchor place: getAnchorsofNamesURL(foodTypeInfo.get("Beverages"))){
+                beverageItem.getChildren().add(new TreeItem<>(name(place)));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TreeItem<String> kosherItem = new TreeItem<>("Kosher Places");
+        try {
+            for (HtmlAnchor place: getAnchorsofNamesURL(foodTypeInfo.get("Kosher Food Available"))){
+                kosherItem.getChildren().add(new TreeItem<>(name(place)));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TreeItem<String> veganItem = new TreeItem<>("Vegan Places");
+        try {
+            for (HtmlAnchor place: getAnchorsofNamesURL(foodTypeInfo.get("Vegan Foods"))){
+                veganItem.getChildren().add(new TreeItem<>(name(place)));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TreeItem<String> vegItem = new TreeItem<>("Vegetarian Places");
+        try {
+            for (HtmlAnchor place: getAnchorsofNamesURL(foodTypeInfo.get("Vegetarian Foods"))){
+                vegItem.getChildren().add(new TreeItem<>(name(place)));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        foodItem.getChildren().addAll(allFoodItem, halalItem, beverageItem,
+                kosherItem, vegItem, veganItem);
 
         rootItem.getChildren().addAll(studyItem, foodItem, buildingItem);
         filterSearch.setRoot(rootItem);
