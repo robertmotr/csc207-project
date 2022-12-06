d9d8879ec9c4b18d8de660aba8b93568cd1013c1
Itsmico66 <krzosmichael@gmail.com>
2022-12-05 22:49:55 -0500

    [DEV-1.5] text to speech speak method

---
+++ TextToSpeech.java
@@ -0,0 +1,25 @@
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
+
+/**
+ * Reference from:
+ * https://stackoverflow.com/questions/34724104/text-to-speech-in-java-using-freetts
+ * https://softwarepulse.co.uk/tutorials/javafx-text-to-speech/
+ */
+public class TextToSpeech {
+    VoiceManager freeVM;
+
+    public static void speak(String words) {
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+        Voice voice = VoiceManager.getInstance().getVoice("kevin16");
+        voice.allocate();
+        voice.setRate(110);
+        voice.setPitch(100);
+        voice.setVolume(5);
+        voice.speak(words);
+    }
+    public static void main(String[] args){
+        TextToSpeech.speak("I would walk five hundred miles and I would walk five hundred more");
+    }
+}
